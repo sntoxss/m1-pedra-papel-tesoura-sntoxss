c6e2c57121336b766c21ba6ee3f66401eaa58bb2
Sntoxss <contato.sntos@gmail.com>
2024-05-23 09:34:44 -0300

    Corrigi alguns erros

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 /* Desenvolva seu código abaixo */
 function playRockPaperScissor(player1, player2) {
-  if (player1 === "Pedra" && player2 === "Tesoura") {
+  if (player1 == "Pedra" && player2 == "Tesoura") {
     return "Jogador 1 venceu!";
-  } else if (player1 === "Pedra" && player2 === "Papel") {
+  } else if (player1 == "Pedra" && player2 == "Papel") {
     return "Jogador 2 venceu!";
-  } else if (player1 === "Pedra" && player2 === "Pedra") {
+  } else if (player1 == "Pedra" && player2 == "Pedra") {
     return "Empate!";
-  } else if (player1 === "Papel" && player2 === "Tesoura") {
+  } else if (player1 == "Papel" && player2 == "Tesoura") {
     return "Jogador 2 venceu!";
-  } else if (player1 === "Papel" && player2 === "Papel") {
+  } else if (player1 == "Papel" && player2 == "Papel") {
     return "Empate!";
-  } else if (player1 === "Papel" && player2 === "Pedra") {
+  } else if (player1 == "Papel" && player2 == "Pedra") {
     return "Jogador 1 venceu!";
-  } else if (player1 === "Tesoura" && player2 === "Pedra") {
+  } else if (player1 == "Tesoura" && player2 == "Pedra") {
     return "Jogador 2 venceu!";
-  } else if (player1 === "Tesoura" && player2 === "Tesoura") {
+  } else if (player1 == "Tesoura" && player2 == "Tesoura") {
     return "Empate!";
-  } else if (player1 === "Tesoura" && player2 === "Papel") {
+  } else if (player1 == "Tesoura" && player2 == "Papel") {
     return "Jogador 1 venceu!";
+  } else {
+    return "resultado inexistente";
   }
-  return "resultado inexistente";
 }
 const resultado = playRockPaperScissor("Pedra", "Pedra");
 console.log(resultado); // Saída: "Jogador 1 venceu!"
